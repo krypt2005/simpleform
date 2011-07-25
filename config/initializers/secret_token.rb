8749d5d7d75ae9d1f29d854ceb5860a20058d77a
krypt2005 <krypt2005@gmail.com>
2011-07-25 19:48:51 +0530

    this is my first app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simpleform::Application.config.secret_token = '89ea493efa7f7429a7524136a4f2ee1dca4c260a090a4ecb67a60a9feed2fac16bb9079f8c1423ae3bda4675bed6aeb8e6cfc3c3b432bb05676ae0c0195650f2'
